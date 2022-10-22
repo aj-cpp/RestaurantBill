de84d12177c6356567ce0faf3e108aee55ccf044
aj-cpp <ajujun@cpp.edu>
2022-10-21 23:06:37 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// CS2600 - Andrew Jujun
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  // Outline:
+  // 1. Create meal struct
+  // 2. Create menu table/map storing meals
+  // 3. Implement random meal logic
+  // 4. Display meal cost, tax amount, tip amount, and total bill 
+
+  /* code */
+  return EXIT_SUCCESS;
+}
+
