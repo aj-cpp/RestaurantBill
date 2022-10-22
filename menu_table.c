3a7271dab614d0c35d995614fe704d817d8b5ab2
aj-cpp <ajujun@cpp.edu>
2022-10-21 23:18:29 -0700

    Add menu table

---
+++ menu_table.c
@@ -0,0 +1,11 @@
+#include "meal.h"
+
+Meal MenuTable[] =
+{
+  {"Salad", 9.95},
+  {"Soup", 4.55},
+  {"Sandwich", 13.25},
+  {"Pizza", 22.35}
+};
+
+const int MENU_SIZE = sizeof(MenuTable)/sizeof(MenuTable[0]);
